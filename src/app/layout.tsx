dd4fd28bd7ff0d0eb9873eec24c585442a99e0b9
mamerisawesome <atmalmer23@gmail.com>
2023-07-07 01:03:28 +0800

    misc: Add robots and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,32 @@
-import './globals.css'
+import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Beshyfy',
   description: 'Transform your sentences with cartwheels',
+  keywords: ['bakit', 'malungkot', 'beshy', 'ko'],
+  openGraph: {
+    title: 'Beshyfy',
+    description: 'Transform your sentences with cartwheels',
+    type: 'website',
+  },
+  authors: [{ name: 'Mamer' }],
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({

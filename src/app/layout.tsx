a6e0d1430bc7a7a74672fc5937742f43a8599c5a
mamerisawesome <atmalmer23@gmail.com>
2023-07-06 20:11:16 +0800

    misc: Update description and package author

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Beshyfy',
-  description: 'Created by Mamer',
+  description: 'Transform your sentences with cartwheels',
 }
 
 export default function RootLayout({
